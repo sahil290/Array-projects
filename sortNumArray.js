901d01c9a0cafdf57d0d5b7917b8c1f702d5103c
sahil290 <sk9969401@gmail.com>
2021-05-18 14:54:31 +0530

    initial commit

---
+++ sortNumArray.js
@@ -0,0 +1,10 @@
+//Q7. Write a JavaScript program to sort the items of an array.//
+//TestData:console.log(sortNumArr([3, 8, 7, 6, 5, -4, 3, 2, 1])); //
+
+
+const sortNumArr = (arr1) => arr1.sort((a, b) => a - b);
+
+
+console.log(sortNumArr([3, 8, 7, 6, 5, -4, 3, 2, 1]));
+
+
---
+++ sortstrArr.js
@@ -0,0 +1,9 @@
+//Q26. Write a JavaScript program to sort the items of an array.//
+//test data//
+//console.log(sortStrArr([“Banana”, “Orange”, “Apple”, “Mango”])); // [Apple, Banana, Mango, Orange]//
+
+
+const sortStrArr = (items) => items.sort();
+
+
+console.log(sortStrArr(["Banana", "Orange", "Apple", "Mango"]));
